9663549335eaa74ff9ec9b3bd52a5e670b6b78d0
Kelly <kellycfjesus2@gmail.com>
2021-05-14 00:45:44 +0100

    ok

---
+++ src/model/StatusRent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author kelly
+ */
+
+//class created to change the movies status ---- not working yet
+public enum StatusRent {
+    
+        rented, returned, lateReturned, soldNotReturned;
+    
+}
