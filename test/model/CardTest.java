5d6ceaf2c11bb09d26ca49030dc409976376e55f
Kelly <kellycfjesus2@gmail.com>
2021-05-15 12:21:20 +0100

    commentcode

---
+++ test/model/CardTest.java
@@ -0,0 +1,173 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.math.BigDecimal;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author kelly
+ */
+public class CardTest {
+    
+    public CardTest() {
+    }
+
+    /**
+     * Test of makePayment method, of class Card.
+     */
+    @Test
+    public void testMakePayment() {
+    }
+
+    /**
+     * Test of getIDcardNumber method, of class Card.
+     */
+    @Test
+    public void testGetIDcardNumber() {
+    }
+
+    /**
+     * Test of setIDcardNumber method, of class Card.
+     */
+    @Test
+    public void testSetIDcardNumber() {
+    }
+
+    /**
+     * Test of getNameCard method, of class Card.
+     */
+    @Test
+    public void testGetNameCard() {
+    }
+
+    /**
+     * Test of setNameCard method, of class Card.
+     */
+    @Test
+    public void testSetNameCard() {
+    }
+
+    /**
+     * Test of getCvv method, of class Card.
+     */
+    @Test
+    public void testGetCvv() {
+    }
+
+    /**
+     * Test of setCvv method, of class Card.
+     */
+    @Test
+    public void testSetCvv() {
+    }
+
+    /**
+     * Test of getMonthExpiresDate method, of class Card.
+     */
+    @Test
+    public void testGetMonthExpiresDate() {
+    }
+
+    /**
+     * Test of setMonthExpiresDate method, of class Card.
+     */
+    @Test
+    public void testSetMonthExpiresDate() {
+    }
+
+    /**
+     * Test of getYearExpiresDate method, of class Card.
+     */
+    @Test
+    public void testGetYearExpiresDate() {
+    }
+
+    /**
+     * Test of setYearExpiresDate method, of class Card.
+     */
+    @Test
+    public void testSetYearExpiresDate() {
+    }
+
+    /**
+     * Test of checkCardValidation method, of class Card.
+     */
+    @Test
+    public void testCheckCardValidation() {
+    }
+
+    /**
+     * Test of cardNameValidation method, of class Card.
+     */
+    @Test
+    public void testCardNameValidation() {
+    }
+
+    /**
+     * Test of idCardNumberValidation method, of class Card.
+     */
+    @Test
+    public void testIdCardNumberValidation() {
+    }
+
+    /**
+     * Test of emptyCardNumberValidation method, of class Card.
+     */
+    @Test
+    public void testEmptyCardNumberValidation() {
+    }
+
+    /**
+     * Test of cardNumberLength method, of class Card.
+     */
+    @Test
+    public void testCardNumberLength() {
+    }
+
+    /**
+     * Test of cvvValidation method, of class Card.
+     */
+    @Test
+    public void testCvvValidation() {
+    }
+
+    /**
+     * Test of cvvLength method, of class Card.
+     */
+    @Test
+    public void testCvvLength() {
+    }
+
+    /**
+     * Test of dateValidation method, of class Card.
+     */
+    @Test
+    public void testDateValidation() {
+    }
+
+    public class CardImpl extends Card {
+
+        public CardImpl() {
+            super("", "", 0, 0, 0);
+        }
+
+        public void makePayment(BigDecimal amount) {
+        }
+
+        @Override
+        public void pay(BigDecimal amount) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+    }
+
+        public void makePayment(BigDecimal amount) {
+        }
+    }
+    
+
